4f901ab086dee475de3a89b6c3fadc6d724783d1
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-22 15:21:52 -0300

    encontrar maior menor e media

--- Programacao_Estruturada/Program.cs
+++ Programacao_Estruturada/Program.cs
@@ -6,7 +6,59 @@ namespace Programacao_Estruturada
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[] numeros = new double[10] { 10, 10, 0, 4, 3, 1, 10, 8, 2, 5 };
+            double maior = 0, menor;
+            double media = 0;
+            double[] tres_maiores = new double[3];
+
+            EncontrarMaiorValor(ref maior, ref numeros);
+            Console.WriteLine("O maior valor é: " + maior);
+
+            EncontrarMenorValor(ref numeros, out menor);
+            Console.WriteLine("O menor valor é: " + menor);
+
+            EncontrarValorMedio(ref media, ref numeros);
+            Console.WriteLine("O valor médio é: " + media);
+
+            EncontrarTresMaioresValores();
+        }
+
+        static void EncontrarMaiorValor(ref double maior_valor, ref double[] array_maior_valor)
+        {
+            maior_valor = -999999999;
+            for (int i = 0; i < 10; i++)
+            {
+                if (array_maior_valor[i] > maior_valor)
+                {
+                    maior_valor = array_maior_valor[i];
+                }
+            }
+        }
+        static void EncontrarMenorValor(ref double[] array_menor_valor, out double menor_valor)
+        {
+            menor_valor = 999999999;
+            for (int i = 0; i < 10; i++)
+            {
+                if (array_menor_valor[i] < menor_valor)
+                {
+                    menor_valor = array_menor_valor[i];
+                }
+            }
+        }
+        static double EncontrarValorMedio(ref double valor_medio, ref double[] array_media)
+        {
+            double somador = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                somador += array_media[i];
+                valor_medio = somador / array_media.Length;
+            }
+            return valor_medio;
+        }
+
+        static void EncontrarTresMaioresValores()
+        {
+
         }
     }
 }
